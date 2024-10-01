8595bf3a0eb3385c4f971b48910d659e38a6a0df
Avatar-cod <148372614+Avatar-cod@users.noreply.github.com>
2024-10-01 16:20:42 +0400

    Add grass

--- Game/GameForm.Designer.cs
+++ Game/GameForm.Designer.cs
@@ -76,6 +76,7 @@ namespace Game
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImage = global::Game.Properties.Resources.grass;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
