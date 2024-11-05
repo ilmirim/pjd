60169e4c605cc63c2da40e0bab4ca8d88a871b68
ilmirim <trash.disp@gmail.com>
2024-11-05 17:11:52 +0400

    addEnterForm

--- Game/Enter.Designer.cs
+++ Game/Enter.Designer.cs
@@ -29,31 +29,31 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.loginText = new System.Windows.Forms.TextBox();
-            this.loginGroup = new System.Windows.Forms.GroupBox();
-            this.passwordGroup = new System.Windows.Forms.GroupBox();
-            this.passwordText = new System.Windows.Forms.TextBox();
-            this.emailGroup = new System.Windows.Forms.GroupBox();
-            this.emailText = new System.Windows.Forms.TextBox();
-            this.cardNumber = new System.Windows.Forms.GroupBox();
-            this.cardNumberText = new System.Windows.Forms.TextBox();
-            this.cardEXPGroup = new System.Windows.Forms.GroupBox();
-            this.cardEXPText = new System.Windows.Forms.TextBox();
             this.CCVGroup = new System.Windows.Forms.GroupBox();
             this.CCVText = new System.Windows.Forms.TextBox();
+            this.cardEXPGroup = new System.Windows.Forms.GroupBox();
+            this.cardEXPText = new System.Windows.Forms.TextBox();
+            this.cardNumber = new System.Windows.Forms.GroupBox();
+            this.cardNumberText = new System.Windows.Forms.TextBox();
+            this.emailGroup = new System.Windows.Forms.GroupBox();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.confirmPassGorup = new System.Windows.Forms.GroupBox();
+            this.confirmPassword = new System.Windows.Forms.TextBox();
+            this.passwordGroup = new System.Windows.Forms.GroupBox();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.loginGroup = new System.Windows.Forms.GroupBox();
+            this.loginText = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.errorText = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.loginGroup.SuspendLayout();
-            this.passwordGroup.SuspendLayout();
-            this.emailGroup.SuspendLayout();
-            this.cardNumber.SuspendLayout();
-            this.cardEXPGroup.SuspendLayout();
             this.CCVGroup.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.cardEXPGroup.SuspendLayout();
+            this.cardNumber.SuspendLayout();
+            this.emailGroup.SuspendLayout();
+            this.confirmPassGorup.SuspendLayout();
+            this.passwordGroup.SuspendLayout();
+            this.loginGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -62,7 +62,7 @@
             this.groupBox1.Controls.Add(this.cardEXPGroup);
             this.groupBox1.Controls.Add(this.cardNumber);
             this.groupBox1.Controls.Add(this.emailGroup);
-            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.confirmPassGorup);
             this.groupBox1.Controls.Add(this.passwordGroup);
             this.groupBox1.Controls.Add(this.loginGroup);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -74,81 +74,24 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "REGISTATION";
             // 
-            // loginText
+            // CCVGroup
             // 
-            this.loginText.Location = new System.Drawing.Point(6, 19);
-            this.loginText.Name = "loginText";
-            this.loginText.Size = new System.Drawing.Size(347, 20);
-            this.loginText.TabIndex = 0;
+            this.CCVGroup.Controls.Add(this.CCVText);
+            this.CCVGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CCVGroup.ForeColor = System.Drawing.Color.White;
+            this.CCVGroup.Location = new System.Drawing.Point(6, 337);
+            this.CCVGroup.Name = "CCVGroup";
+            this.CCVGroup.Size = new System.Drawing.Size(359, 47);
+            this.CCVGroup.TabIndex = 2;
+            this.CCVGroup.TabStop = false;
+            this.CCVGroup.Text = "CARD CCV";
             // 
-            // loginGroup
+            // CCVText
             // 
-            this.loginGroup.Controls.Add(this.loginText);
-            this.loginGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.loginGroup.ForeColor = System.Drawing.Color.White;
-            this.loginGroup.Location = new System.Drawing.Point(6, 19);
-            this.loginGroup.Name = "loginGroup";
-            this.loginGroup.Size = new System.Drawing.Size(359, 47);
-            this.loginGroup.TabIndex = 1;
-            this.loginGroup.TabStop = false;
-            this.loginGroup.Text = "LOGIN";
-            // 
-            // passwordGroup
-            // 
-            this.passwordGroup.Controls.Add(this.passwordText);
-            this.passwordGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.passwordGroup.ForeColor = System.Drawing.Color.White;
-            this.passwordGroup.Location = new System.Drawing.Point(6, 72);
-            this.passwordGroup.Name = "passwordGroup";
-            this.passwordGroup.Size = new System.Drawing.Size(359, 47);
-            this.passwordGroup.TabIndex = 2;
-            this.passwordGroup.TabStop = false;
-            this.passwordGroup.Text = "PASSWORD";
-            // 
-            // passwordText
-            // 
-            this.passwordText.Location = new System.Drawing.Point(6, 19);
-            this.passwordText.Name = "passwordText";
-            this.passwordText.Size = new System.Drawing.Size(347, 20);
-            this.passwordText.TabIndex = 0;
-            // 
-            // emailGroup
-            // 
-            this.emailGroup.Controls.Add(this.emailText);
-            this.emailGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.emailGroup.ForeColor = System.Drawing.Color.White;
-            this.emailGroup.Location = new System.Drawing.Point(6, 178);
-            this.emailGroup.Name = "emailGroup";
-            this.emailGroup.Size = new System.Drawing.Size(359, 47);
-            this.emailGroup.TabIndex = 2;
-            this.emailGroup.TabStop = false;
-            this.emailGroup.Text = "EMAIL";
-            // 
-            // emailText
-            // 
-            this.emailText.Location = new System.Drawing.Point(6, 19);
-            this.emailText.Name = "emailText";
-            this.emailText.Size = new System.Drawing.Size(347, 20);
-            this.emailText.TabIndex = 0;
-            // 
-            // cardNumber
-            // 
-            this.cardNumber.Controls.Add(this.cardNumberText);
-            this.cardNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.cardNumber.ForeColor = System.Drawing.Color.White;
-            this.cardNumber.Location = new System.Drawing.Point(6, 231);
-            this.cardNumber.Name = "cardNumber";
-            this.cardNumber.Size = new System.Drawing.Size(359, 47);
-            this.cardNumber.TabIndex = 2;
-            this.cardNumber.TabStop = false;
-            this.cardNumber.Text = "CARD NUMBER";
-            // 
-            // cardNumberText
-            // 
-            this.cardNumberText.Location = new System.Drawing.Point(6, 19);
-            this.cardNumberText.Name = "cardNumberText";
-            this.cardNumberText.Size = new System.Drawing.Size(347, 20);
-            this.cardNumberText.TabIndex = 0;
+            this.CCVText.Location = new System.Drawing.Point(6, 19);
+            this.CCVText.Name = "CCVText";
+            this.CCVText.Size = new System.Drawing.Size(347, 20);
+            this.CCVText.TabIndex = 0;
             // 
             // cardEXPGroup
             // 
@@ -169,24 +112,100 @@
             this.cardEXPText.Size = new System.Drawing.Size(347, 20);
             this.cardEXPText.TabIndex = 0;
             // 
-            // CCVGroup
+            // cardNumber
             // 
-            this.CCVGroup.Controls.Add(this.CCVText);
-            this.CCVGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.CCVGroup.ForeColor = System.Drawing.Color.White;
-            this.CCVGroup.Location = new System.Drawing.Point(6, 337);
-            this.CCVGroup.Name = "CCVGroup";
-            this.CCVGroup.Size = new System.Drawing.Size(359, 47);
-            this.CCVGroup.TabIndex = 2;
-            this.CCVGroup.TabStop = false;
-            this.CCVGroup.Text = "CARD CCV";
+            this.cardNumber.Controls.Add(this.cardNumberText);
+            this.cardNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cardNumber.ForeColor = System.Drawing.Color.White;
+            this.cardNumber.Location = new System.Drawing.Point(6, 231);
+            this.cardNumber.Name = "cardNumber";
+            this.cardNumber.Size = new System.Drawing.Size(359, 47);
+            this.cardNumber.TabIndex = 2;
+            this.cardNumber.TabStop = false;
+            this.cardNumber.Text = "CARD NUMBER";
             // 
-            // CCVText
+            // cardNumberText
             // 
-            this.CCVText.Location = new System.Drawing.Point(6, 19);
-            this.CCVText.Name = "CCVText";
-            this.CCVText.Size = new System.Drawing.Size(347, 20);
-            this.CCVText.TabIndex = 0;
+            this.cardNumberText.Location = new System.Drawing.Point(6, 19);
+            this.cardNumberText.Name = "cardNumberText";
+            this.cardNumberText.Size = new System.Drawing.Size(347, 20);
+            this.cardNumberText.TabIndex = 0;
+            // 
+            // emailGroup
+            // 
+            this.emailGroup.Controls.Add(this.emailText);
+            this.emailGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emailGroup.ForeColor = System.Drawing.Color.White;
+            this.emailGroup.Location = new System.Drawing.Point(6, 178);
+            this.emailGroup.Name = "emailGroup";
+            this.emailGroup.Size = new System.Drawing.Size(359, 47);
+            this.emailGroup.TabIndex = 2;
+            this.emailGroup.TabStop = false;
+            this.emailGroup.Text = "EMAIL";
+            // 
+            // emailText
+            // 
+            this.emailText.Location = new System.Drawing.Point(6, 19);
+            this.emailText.Name = "emailText";
+            this.emailText.Size = new System.Drawing.Size(347, 20);
+            this.emailText.TabIndex = 0;
+            // 
+            // confirmPassGorup
+            // 
+            this.confirmPassGorup.Controls.Add(this.confirmPassword);
+            this.confirmPassGorup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.confirmPassGorup.ForeColor = System.Drawing.Color.White;
+            this.confirmPassGorup.Location = new System.Drawing.Point(6, 125);
+            this.confirmPassGorup.Name = "confirmPassGorup";
+            this.confirmPassGorup.Size = new System.Drawing.Size(359, 47);
+            this.confirmPassGorup.TabIndex = 2;
+            this.confirmPassGorup.TabStop = false;
+            this.confirmPassGorup.Text = "CONFIRM PASSWORD";
+            // 
+            // confirmPassword
+            // 
+            this.confirmPassword.Location = new System.Drawing.Point(6, 19);
+            this.confirmPassword.Name = "confirmPassword";
+            this.confirmPassword.Size = new System.Drawing.Size(347, 20);
+            this.confirmPassword.TabIndex = 0;
+            // 
+            // passwordGroup
+            // 
+            this.passwordGroup.Controls.Add(this.passwordText);
+            this.passwordGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordGroup.ForeColor = System.Drawing.Color.White;
+            this.passwordGroup.Location = new System.Drawing.Point(6, 72);
+            this.passwordGroup.Name = "passwordGroup";
+            this.passwordGroup.Size = new System.Drawing.Size(359, 47);
+            this.passwordGroup.TabIndex = 2;
+            this.passwordGroup.TabStop = false;
+            this.passwordGroup.Text = "PASSWORD";
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(6, 19);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(347, 20);
+            this.passwordText.TabIndex = 0;
+            // 
+            // loginGroup
+            // 
+            this.loginGroup.Controls.Add(this.loginText);
+            this.loginGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginGroup.ForeColor = System.Drawing.Color.White;
+            this.loginGroup.Location = new System.Drawing.Point(6, 19);
+            this.loginGroup.Name = "loginGroup";
+            this.loginGroup.Size = new System.Drawing.Size(359, 47);
+            this.loginGroup.TabIndex = 1;
+            this.loginGroup.TabStop = false;
+            this.loginGroup.Text = "LOGIN";
+            // 
+            // loginText
+            // 
+            this.loginText.Location = new System.Drawing.Point(6, 19);
+            this.loginText.Name = "loginText";
+            this.loginText.Size = new System.Drawing.Size(347, 20);
+            this.loginText.TabIndex = 0;
             // 
             // button1
             // 
@@ -210,25 +229,6 @@
             this.errorText.TabIndex = 2;
             this.errorText.Text = "ERROR";
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.textBox1);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.groupBox2.ForeColor = System.Drawing.Color.White;
-            this.groupBox2.Location = new System.Drawing.Point(6, 125);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(359, 47);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "PASSWORD";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(6, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(347, 20);
-            this.textBox1.TabIndex = 0;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -238,6 +238,7 @@
             this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "у меня уже есть аккаунт";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Enter
             // 
@@ -253,20 +254,20 @@
             this.Name = "Enter";
             this.Text = "Enter";
             this.groupBox1.ResumeLayout(false);
-            this.loginGroup.ResumeLayout(false);
-            this.loginGroup.PerformLayout();
-            this.passwordGroup.ResumeLayout(false);
-            this.passwordGroup.PerformLayout();
-            this.emailGroup.ResumeLayout(false);
-            this.emailGroup.PerformLayout();
-            this.cardNumber.ResumeLayout(false);
-            this.cardNumber.PerformLayout();
-            this.cardEXPGroup.ResumeLayout(false);
-            this.cardEXPGroup.PerformLayout();
             this.CCVGroup.ResumeLayout(false);
             this.CCVGroup.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
+            this.cardEXPGroup.ResumeLayout(false);
+            this.cardEXPGroup.PerformLayout();
+            this.cardNumber.ResumeLayout(false);
+            this.cardNumber.PerformLayout();
+            this.emailGroup.ResumeLayout(false);
+            this.emailGroup.PerformLayout();
+            this.confirmPassGorup.ResumeLayout(false);
+            this.confirmPassGorup.PerformLayout();
+            this.passwordGroup.ResumeLayout(false);
+            this.passwordGroup.PerformLayout();
+            this.loginGroup.ResumeLayout(false);
+            this.loginGroup.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -289,8 +290,8 @@
         private System.Windows.Forms.TextBox CCVText;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label errorText;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox confirmPassGorup;
+        private System.Windows.Forms.TextBox confirmPassword;
         private System.Windows.Forms.Label label1;
     }
 }
--- Game/Enter.cs
+++ Game/Enter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,77 +9,169 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace Game
 {
     public partial class Enter : Form
     {
+        private bool isLogin;
+        private string path;
+
         public Enter()
         {
             InitializeComponent();
 
             errorText.Text = "";
+            isLogin = false;
+            path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//pjd Project//loginData";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(loginText.Text.Length > 12 )
+
+            if (!isLogin)
             {
-                errorText.Text = "Длинна логина больше 12 символов";
-                return;
+                if (loginText.Text.Length > 12)
+                {
+                    errorText.Text = "Длинна логина больше 12 символов";
+                    return;
+                }
+                else if (loginText.Text.Length == 0)
+                {
+                    errorText.Text = "Длинна логина равна 0 символам";
+                    return;
+                }
+
+                if (passwordText.Text != confirmPassword.Text)
+                {
+                    errorText.Text = "Пароли не совпадают";
+                    return;
+                }
+
+                if (passwordText.Text.Length > 20)
+                {
+                    errorText.Text = "Длинна пароля больше 20 символов";
+                    return;
+                }
+                else if (passwordText.Text.Length < 8)
+                {
+                    errorText.Text = "Длинна пароля меньше 8 символам";
+                    return;
+                }
+
+                Regex emailRegex = new Regex(@"\S+@\S+\.((by)|(ru)|(com))");
+                Match match = emailRegex.Match(emailText.Text);
+                if (!match.Success)
+                {
+                    errorText.Text = "Неправильный формат почты";
+                    return;
+                }
+
+                Regex cardNumberRegex = new Regex(@"\d{4}-\d{4}-\d{4}-\d{4}");
+                match = cardNumberRegex.Match(cardNumberText.Text);
+                if (!match.Success)
+                {
+                    errorText.Text = "Неправильный формат номера карты";
+                    return;
+                }
+
+                Regex cardEXPRegex = new Regex("((0[1-9])|(1[0-2]))/[0-9]{2}");
+                match = cardEXPRegex.Match(cardEXPText.Text);
+                if (!match.Success)
+                {
+                    errorText.Text = "Неправильный формат срока годности карты";
+                    return;
+                }
+
+                DateTime dateTime = DateTime.ParseExact(match.Value, "MM/yy", CultureInfo.InvariantCulture);
+
+                if (dateTime <= DateTime.Now)
+                {
+                    errorText.Text = "Неправильный формат срока годности карты";
+                    return;
+                }
+
+                Regex ccvRegex = new Regex("[0-9]{3}");
+                match = ccvRegex.Match(CCVText.Text);
+                if (!match.Success)
+                {
+                    errorText.Text = "Неправильный формат CCV кода";
+                    return;
+                }
+
+                errorText.Text = "";
+
+                if (IsUserExist())
+                {
+                    errorText.Text = "Логин занят";
+                    return;
+                }
+
+                var textToFile = $"{loginText.Text}_{passwordText.Text}_{emailText.Text}_{cardNumberText.Text}_{cardEXPText.Text}_{CCVText.Text}";
+                File.AppendAllText(path, textToFile);
+
+                Close();
             }
-            else if(loginText.Text.Length == 0)
+            else
             {
-                errorText.Text = "Длинна логина равна 0 символам";
-                return;
+                var allData = File.ReadAllLines(path);
+
+                var flag = false;
+                foreach (var data in allData)
+                {
+                    if (loginText.Text == data.Split('_')[0] &&
+                        passwordText.Text == data.Split('_')[1])
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag)
+                {
+                    errorText.Text = "Неправильный логин или пароль";
+                    return;
+                }
+                else Close();
+            }
+        }
+
+        private bool IsUserExist()
+        {
+            var allData = File.ReadAllLines(path);
+
+            foreach (var data in allData)
+            {
+                if (loginText.Text == data.Split('_')[0])
+                    return true;
+            }
+            return false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            isLogin = !isLogin;
+
+            if (isLogin)
+            {
+                label1.Text = "У меня нет аккаунта";
+                confirmPassGorup.Visible = false;
+                emailGroup.Visible = false;
+                cardNumber.Visible = false;
+                cardEXPGroup.Visible = false;
+                CCVGroup.Visible = false;
+            }
+            else
+            {
+                label1.Text = "У меня уже есть аккаунт";
+                confirmPassGorup.Visible = true;
+                emailGroup.Visible = true;
+                cardNumber.Visible = true;
+                cardEXPGroup.Visible = true;
+                CCVGroup.Visible = true;
             }
 
-            if (passwordText.Text.Length > 20)
-            {
-                errorText.Text = "Длинна пароля больше 20 символов";
-                return;
-            }
-            else if (passwordText.Text.Length < 8)
-            {
-                errorText.Text = "Длинна пароля меньше 8 символам";
-                return;
-            }
-
-            Regex emailRegex = new Regex("\\S+@\\S+\\.\\S+"); 
-            Match match = emailRegex.Match(emailText.Text);
-            if(!match.Success)
-            {
-                errorText.Text = "Неправильный формат почты";
-                return;
-            }
-
-            Regex cardNumberRegex = new Regex(@"\d{4}-\d{4}-\d{4}-\d{4}");
-            match = cardNumberRegex.Match(cardNumberText.Text);
-            if (!match.Success)
-            {
-                errorText.Text = "Неправильный формат номера карты";
-                return;
-            }
-
-            Regex cardEXPRegex = new Regex("[0-9]{2}/[0-9]{2}");
-            match = cardEXPRegex.Match(cardEXPText.Text);
-            if (!match.Success)
-            {
-                errorText.Text = "Неправильный формат срока годности карты";
-                return;
-            }
-
-            Regex ccvRegex = new Regex("[0-9]{3}");
-            match = ccvRegex.Match(CCVText.Text);
-            if (!match.Success)
-            {
-                errorText.Text = "Неправильный формат CCV кода";
-                return;
-            }
-
-            errorText.Text = "";
-
-            Close();
         }
     }
 }
